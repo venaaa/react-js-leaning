d77c1c4640d119fbbb4eaafbbae580b9b48f6f5a
Serhii <idaveee@gmail.com>
2020-10-18 01:00:23 +0300

    lesson 14. added 014.js

---
+++ Main/js/014-script.js
@@ -0,0 +1,24 @@
+
+
+let num = 50;
+
+// while (num <= 60) {
+//     console.log(num);
+//     num++;
+// }
+
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if ( i === 6) {
+        // break;
+        continue;
+    }
+
+    console.log(i);
+}
