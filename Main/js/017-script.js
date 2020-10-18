9964e1a595e532c35a333775871c3febc638b950
Serhii <idaveee@gmail.com>
2020-10-19 01:05:39 +0300

    lesson 17 done. 017.js added

---
+++ Main/js/017-script.js
@@ -0,0 +1,24 @@
+
+
+
+const str = "test";
+const arr = [1, 2, 4];
+
+console.log(str.toLocaleUpperCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello World";
+console.log(logg.slice(0, 6));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 3));
+
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
