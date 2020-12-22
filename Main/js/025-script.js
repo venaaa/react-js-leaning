351d16b93bdded0c6567cd1f828b3a9483a2d263
Serhii <idaveee@gmail.com>
2020-12-22 23:34:02 +0300

    lesson 025 DONE. google chrome dev tools (sources)

---
+++ Main/js/025-script.js
@@ -0,0 +1,20 @@
+
+
+function hello() {
+    console.log("Hello World");
+}
+
+hello();
+
+function hi() {
+    console.log('Say hi!');
+}
+
+hi();
+
+const arr = [1, 14, 4, 30, 54],
+      sorted = arr.sort(compareNum);
+
+function compareNum(a, b) {
+    return a - b;
+}
