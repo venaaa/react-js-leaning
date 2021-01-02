048a19d666b7c7f7b4a864c4c3e0baecb33fa6b4
Serhii <idaveee@gmail.com>
2021-01-02 16:41:04 +0300

    lesson 32 done. dom-navigation. 032.js added

---
+++ Main/js/032-script.js
@@ -0,0 +1,20 @@
+
+
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes); // Позволяет получить все узлы-ноды внутри родителя
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+
+    console.log(node);
+}
