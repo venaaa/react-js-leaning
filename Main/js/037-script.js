0f65de8511189ad1f2c8b87bcad62bc70281a52c
Serhii <idaveee@gmail.com>
2021-01-03 21:17:37 +0300

    lesson 37 done. delegate. 037.js added.

---
+++ Main/js/037-script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[1].classList.add('red'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
+
+
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+
+    btns[1].classList.toggle('red');
+});
+
+// console.log(btns[0].className);
+
+//ДЕЛЕГИРОВАНИЕ
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches("button.red")) {
+        console.log('Hello');
+    }
+});
+
+//НЕ ДЕЛЕГИРОВАНИЕ
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
