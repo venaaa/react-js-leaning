a713c2fbe22c292745f9f73c00ecca296e70ee34
Serhii <idaveee@gmail.com>
2021-03-04 20:42:23 +0300

    lesson 67 done. localstorage. git going w/o appending script files

---
+++ Main/js/065-regular.js
@@ -0,0 +1,32 @@
+    // REGULAR EXP
+    // регулярные выражения
+    // new RegExp('pattern', 'flags');
+
+// /pattern/f
+
+// const ans = prompt('Your digit: ');
+
+// const reg = /\d/g;
+// console.log(ans.match(reg));
+
+// const str = 'My name is R2D2';
+// console.log(str.match(/\D/ig));
+
+// \D
+// \W
+
+// \d
+// \w
+// \s
+
+// i не зависит от регистра
+// g глобал, несколько вхождений
+// m многострочный режим
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password: ');
+// console.log(pass.replace(/./g, '*'));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
