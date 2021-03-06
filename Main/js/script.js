ebf512b39797e987ea54a84eaf69ad3623b6642a
Serhii <idaveee@gmail.com>
2021-03-07 00:36:04 +0300

    069 INCAPSULATION.

--- Main/js/script.js
+++ Main/js/script.js
@@ -19,3 +19,73 @@
 // console.log(persone.userAge);
 
 // ========= / GETTER/SETTER 068 ============
+
+// ========= 069 INCAPSULATION ============
+
+// FUNCTIONS
+// function User(name, age) {
+// 	this.name = name;
+// 	let userAge = age;
+
+// 	this.say = function() {
+// 		console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+// 	};
+
+// 	this.getAge = function() {
+// 		return userAge;
+// 	};
+
+// 	this.setAge = function(age) {
+// 		if (typeof age === 'number' && age > 0 && age < 110) {
+// 			userAge = age;
+// 		} else {
+// 			console.log('Недопустимое значение');
+// 		}
+// 	};
+// }
+
+// const serge = new User('Serge', 27);
+// console.log(serge.name);
+// console.log(serge.getAge()); 
+
+// serge.setAge(30);
+// serge.setAge(300);
+// console.log(serge.getAge());
+
+// serge.say();
+
+// CLASSES
+// class User {
+// 	constructor(name, age) {
+// 		this.name = name;
+// 		this._age = age;
+// 	}
+
+// 	#surname = 'Brewyak';
+
+// 	say = () => {
+// 		console.log(`Имя пользователя: ${this.name}${this.#surname}, возраст ${this._age}`);
+// 	}
+
+// 	get age() {
+// 		return this._age;
+// 	}
+
+// 	set age(age) {
+// 		if (typeof age === 'number' && age > 0 && age < 110) {
+// 			this._age = age;
+// 		} else {
+// 			console.log('Недопустимое значение');
+// 		}
+// 	}
+// }
+
+// const serge = new User('Serge', 27);
+
+// // console.log(serge.age);
+// // serge.age = 99;
+// // console.log(serge.age);
+// console.log(serge.surname);
+// serge.say();
+
+// ========= / 069 INCAPSULATION ============
