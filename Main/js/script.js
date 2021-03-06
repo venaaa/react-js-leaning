f271f31b37586b8d8700e5ae34c15d45df8fe48d
Serhii <idaveee@gmail.com>
2021-03-07 00:01:32 +0300

    lesson 68 done. getters and setters.

--- Main/js/script.js
+++ Main/js/script.js
@@ -1,32 +1,21 @@
 'use strict';
 
-// new RegExp('pattern', 'flags');
+// ========= GETTER/SETTER 068 ===========
 
-// /pattern/f
+// const persone = {
+// 	name: 'Alex',
+// 	age: 25,
 
-// const ans = prompt('Your digit: ');
+// 	get userAge() {
+// 		return this.age;
+// 	},
 
-// const reg = /\d/g;
-// console.log(ans.match(reg));
+// 	set userAge(num) {
+// 		this.age = num;
+// 	}
+// };
 
-const str = 'My name is R2D2';
-console.log(str.match(/\D/ig));
+// console.log(persone.userAge = 30); //когда вызываем геттер, в конце не ставим круглые скобки.
+// console.log(persone.userAge);
 
-// \D
-// \W
-
-// \d
-// \w
-// \s
-
-// i не зависит от регистра
-// g глобал, несколько вхождений
-// m многострочный режим
-
-// console.log(ans.search(reg));
-// console.log(ans.match(reg));
-
-// const pass = prompt('Password: ');
-// console.log(pass.replace(/./g, '*'));
-
-// console.log('12-34-56'.replace(/-/g, ':'));
+// ========= / GETTER/SETTER 068 ============
