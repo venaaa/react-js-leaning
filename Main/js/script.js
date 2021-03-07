da1ff5215533a9f571e4ad4a9018696e68f88ef3
Serhii <idaveee@gmail.com>
2021-03-07 15:47:50 +0300

    lesson 70 done. modules and privat functions.

--- Main/js/script.js
+++ Main/js/script.js
@@ -1,3 +1,4 @@
+/*jslint node: true */
 'use strict';
 
 // ========= GETTER/SETTER 068 ===========
@@ -89,3 +90,31 @@
 // serge.say();
 
 // ========= / 069 INCAPSULATION ============
+
+// ========= 070 МОДУЛИ ===============
+
+// const app = '123';
+
+// const number = 1;
+
+// (function(){
+// 	let number = 2;
+// 	console.log(number);
+// 	console.log(number + 3);
+// }());
+
+// console.log(number);
+
+// const user = (function () {
+// 	const privat = function () {
+// 		console.log('I am privat!');
+// 	};
+
+// 	return {
+// 		sayHello: privat
+// 	};
+// }());
+
+// user.sayHello();
+
+// ========= / 070 МОДУЛИ ===============
